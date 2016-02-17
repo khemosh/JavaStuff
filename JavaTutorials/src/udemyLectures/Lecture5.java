79b9f977e79c28bcab2d08813bb8322ca1b3898d
bcowzer <bcowzer@gmail.com>
2016-02-17 14:39:09 +0000

    tutorial updates

--- JavaTutorials/src/udemyLectures/Lecture5.java
+++ JavaTutorials/src/udemyLectures/Lecture5.java
@@ -5,7 +5,27 @@ package udemyLectures;
 public class Lecture5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// types of primitive variables
+
+		int myInt = 5;
+		double myDouble = 3.44;
+		float myFloat = 5.66f;
+		short myShort = 43;
+		long myLong = 345464;
+		byte myByte = 34;
+
+		char myChar = 'f';
+
+		boolean myBool = true;
+		
+		// String assignment and equality
+		String one = new String("one");
+		String two = "one";
+		String three = "one";
+		
+		System.out.println(two.equals(one)); // two has the same value as one
+		System.out.println(two == one); // but two does not point to the same thing as one ??
+		System.out.println(two == three); // but here two points to the same thing as three cos the way it was assigned!!!
 
 	}
 
